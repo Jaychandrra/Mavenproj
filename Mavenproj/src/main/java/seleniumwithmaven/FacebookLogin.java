10caa50248d86121d36636c2e776524468a874e1
KDKKUMAR <110330099+KDKKUMAR@users.noreply.github.com>
2023-07-14 11:40:52 +0530

    Update FacebookLogin.java
    commit kdkd1

--- Mavenproj/src/main/java/seleniumwithmaven/FacebookLogin.java
+++ Mavenproj/src/main/java/seleniumwithmaven/FacebookLogin.java
@@ -19,7 +19,7 @@ public class FacebookLogin {
 
     System.setProperty("webdriver.chrome.driver","C:\\projects QA\\MavenProject\\chrome drivers\\chromedriver_win32//chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
-	driver.navigate().to("https://www.google.com/");
+	driver.().to("https://www.google.com/");
 	driver.manage().window().maximize();
 	driver.navigate().to("https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525805424%7Cb%7Cfacebook%20com%20%23%7C&placement=&creative=550525805424&keyword=facebook%20com%20%23&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696221352%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-498889442776%26loc_physical_ms%3D9050516%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMImZCvy_-J-wIVyRwrCh0iSg_WEAAYASAAEgJbzPD_BwE");
 	 driver.findElement(By.name("firstname")).sendKeys("jayachandrra");
